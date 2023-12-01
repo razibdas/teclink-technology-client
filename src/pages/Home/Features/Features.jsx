23ecb10615d16c4e28a4158e1d82ef5e67f13b05
razibraj <razibraj56@gmail.com>
2023-12-02 01:31:19 +0600

    create the featuer ititem

--- src/pages/Home/Features/Features.jsx
+++ src/pages/Home/Features/Features.jsx
@@ -1,14 +1,14 @@
 
 const Features = ({ feature }) => {
-    const {name} = feature;
+    const {name, image, price} = feature;
     return (
         <div className="card card-compact bg-base-100 shadow-xl">
-            <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+            <figure><img src={image} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                <p>Price: ${price}</p>
+                <div className="card-actions w-full">
+                    <button className="btn btn-secondary w-full">Add Product</button>
                 </div>
             </div>
         </div>
--- src/pages/Home/TrendingProducts/TrendingProducts.jsx
+++ src/pages/Home/TrendingProducts/TrendingProducts.jsx
@@ -10,7 +10,7 @@ const TrendingProducts = () => {
     return (
         <div className="">
             <Banners></Banners>
-            <div className="">
+            <div className="mt-12">
                 <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-16">
                     {
                         trendings.map(trending => <TrendingCard key={trending.id} trending={trending}></TrendingCard>)
