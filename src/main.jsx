04b5ff65d61aed282ec1e8bc98b39c1d7b285a24
razibraj <razibraj56@gmail.com>
2023-12-02 22:10:02 +0600

    create the tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,21 @@ import {
 import { router } from './Routes/Routes';
 import AuthProvider from './provider/AuthProvider';
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <div className='max-w-screen-xl mx-auto'>
-        <RouterProvider router={router} />
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className='max-w-screen-xl mx-auto'>
+          <RouterProvider router={router} />
+        </div>
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>,
 )
